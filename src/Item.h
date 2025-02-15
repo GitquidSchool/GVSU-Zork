777fcf9353bf23717c1596d95cf1bb322575de42
Vincent Nguyen <nguyenv2@mail.gvsu.edu>
2025-02-15 17:21:46 -0500

    Implementing Item header, intending to work on actual Item class next.

---
+++ src/Item.h
@@ -0,0 +1,31 @@
+#ifndef ITEM_H 
+#define ITEM_H
+
+#include <string>   
+#include <iostream> 
+#include <stdexcept> 
+
+class Item {
+private:
+    std::string name;           // Name (cannot be empty)
+    std::string description;    // Description (cannot be empty)
+    int calories;               // (0-1000)
+    float weight;               // (0-500lbs)
+
+public:
+    // Constructor to initialize an Item with its name, description, calories, and weight
+    // Throws exceptions if any argument is invalid
+    Item(const std::string &name, const std::string &description,
+         int calories, float weight);
+
+    std::string get_name() const;   
+    std::string get_description() const; 
+    int get_calories() const;      
+    float get_weight() const;      
+
+    // Overload the stream insertion operator to allows the Item class to be printed
+    friend std::ostream& operator<<(std::ostream& os, const Item& item);
+};
+
+#endif  
+
